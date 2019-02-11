7ad21a3f26e16fc80868cb3915474420abe86963
Yaci016 <yacinepro69@gmail.com>
2019-02-11 14:54:29 +0100

    Rajout d'une functionalité

--- Premiere_Appli_node.js/server.js
+++ Premiere_Appli_node.js/server.js
@@ -1,7 +1,27 @@
 let http = require('http');
+let url = require('url');
+let queryString = require('querystring');
 
 let server = http.createServer((req, res) => {
+    let page = url.parse(req.url).pathname;
+    let params = queryString.parse(url.parse(req.url).query);
+    console.log('name' in params);
     res.writeHead(200,{"Content-Type":"text/html"});
-    res.end('<h1>Hello World ! </h1>');
+    switch (page) {
+        case '/': case '/home':
+        res.write("<h1>Hello World ! </h1>");
+        break;
+        case 'nom':
+        if ('name' in params) {
+            res.write("<h1>Hello "+ params['name']+" ! </h1>");
+        } else {
+            res.write("<h1>Hello annonymous ! </h1>");
+        }
+        break;
+        default : 
+        res.write('404 error. Cette page n\'existe pas : '+page);
+        break;
+    }
+    res.end('');
 });
 server.listen(8080);
